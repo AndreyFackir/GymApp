1f4d2c1b4a072e2f3e4462f19504f877351eed17
AndreyFackir <104596263+AndreyFackir@users.noreply.github.com>
2022-06-12 17:11:20 +0300

    add textField

--- Gym/Controllers/NewWorkoutViewController.swift
+++ Gym/Controllers/NewWorkoutViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class NewWorkoutViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setConstraints()
+    }
+    
     private let headerLabel: UILabel = {
        let label = UILabel()
         label.text =  "NEW WORKOUT"
@@ -18,18 +24,56 @@ class NewWorkoutViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupView()
-        setConstraints()
+    
+    private let closeButton: UIButton = {
+       let button =  UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Close Button"), for: .normal)
+        button.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func closeButtonTapped() {
+         dismiss(animated: true, completion: nil)
     }
+    
+    override func viewDidLayoutSubviews() {
+        closeButton.layer.cornerRadius = closeButton.frame.height / 2
+    }
+
+    private let nameLabel: UILabel = {
+       let name = UILabel()
+        name.translatesAutoresizingMaskIntoConstraints = false
+        name.text = "Name"
+        name.textColor = .specilaLightBrown
+        name.font = .robotoMedium12()
+        return name
+    }()
+    
+    private let nameTextField: UITextField = {
+       let text = UITextField()
+        text.clearButtonMode = .always
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.backgroundColor = .specialBrown
+        text.layer.cornerRadius = 10
+        text.font = .robotoBold20()
+        text.textColor = .specialGray
+        text.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: text.frame.height))
+        text.leftViewMode = .always
+        text.returnKeyType = .done
+        return text
+    }()
+    
+    
     
     
     
     private func setupView() {
         view.backgroundColor = .specialbackground
         view.addSubview(headerLabel)
+        view.addSubview(closeButton)
+        view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
     }
     
    
@@ -40,10 +84,31 @@ extension NewWorkoutViewController {
         
         NSLayoutConstraint.activate([
         
-            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-           
-            
+        ])
+        
+        NSLayoutConstraint.activate([
+        
+            closeButton.centerYAnchor.constraint(equalTo: headerLabel.centerYAnchor, constant: 0),
+            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            closeButton.heightAnchor.constraint(equalToConstant: 30),
+            closeButton.widthAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        NSLayoutConstraint.activate([
+        
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+        
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            nameTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            nameTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            nameTextField.heightAnchor.constraint(equalToConstant: 38)
+        
         ])
     }
 }
