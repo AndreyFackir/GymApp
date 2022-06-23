1ac367942c30103107efc52d808c608927708ec8
AndreyFackir <104596263+AndreyFackir@users.noreply.github.com>
2022-06-23 20:39:24 +0300

    add RealmManager

---
+++ Gym/Resources/Services/RealmManager.swift
@@ -0,0 +1,16 @@
+//
+//  RealmManager.swift
+//  Gym
+//
+//  Created by Андрей Яфаркин on 23.06.2022.
+//
+
+import Foundation
+
+class RealmManager {
+    
+    //singleton
+    static let shared = RealmManager()
+    
+    
+}
