625187307f259e1dbcbb1d5a11f873cbdd3149bb
AndreyFackir <104596263+AndreyFackir@users.noreply.github.com>
2022-06-27 22:36:44 +0300

    timerWorkoutVC initial

---
+++ Gym/Controllers/TimerWorkoutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TimerWorkoutViewController.swift
+//  Gym
+//
+//  Created by Андрей Яфаркин on 27.06.2022.
+//
+
+import UIKit
+
+class TimerWorkoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setConstraints()
+        
+    }
+    
+    
+    
+    
+}
+
+//MARK: - Setup Views
+extension TimerWorkoutViewController {
+    private func setupViews() {
+        
+    }
+}
+
+//MARK: - SetConstraints
+extension TimerWorkoutViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
